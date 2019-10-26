9c7f8cafe899d970b08187d5df41d6f6bd18e34c
nunorrodrigues <nuno.rodrigues@jci.com>
2019-10-26 18:38:16 +0100

    exercicio1

--- src/main/java/BookRunner.java
+++ src/main/java/BookRunner.java
@@ -4,19 +4,18 @@ public class BookRunner {
 
     public static void main(String[] args)
     {
-        //Exercicio 1
+        System.out.println("Exercicio 1");
+
         Book book1 = new Book();
         book1.setTitle("The Art Of Computer Programming");
-
         Book book2 = new Book();
         book2.setTitle("Effective Java");
-
         Book book3 = new Book();
         book3.setTitle("Clean Code");
 
-        System.out.println(String.format( "O nome do livro é %s", book1.getTitle()));
-        System.out.println(String.format( "O nome do livro é %s", book2.getTitle()));
-        System.out.println(String.format( "O nome do livro é %s", book3.getTitle()));
+        System.out.println(String.format( "Name: %s", book1.getTitle()));
+        System.out.println(String.format( "Name: %s", book2.getTitle()));
+        System.out.println(String.format( "Name: %s", book3.getTitle()));
 
 
 
