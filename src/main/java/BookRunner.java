3edbb7723022c195308e0037ac3d6ba5d2ec7a53
Sergio <sergioocardoso08@gmail.com>
2019-10-28 20:19:45 +0000

    remove code

--- src/main/java/BookRunner.java
+++ src/main/java/BookRunner.java
@@ -1,7 +1,4 @@
-import models.Author;
 import models.Book;
-import models.Review;
-import models.Student;
 
 public class BookRunner {
 
@@ -9,67 +6,15 @@ public class BookRunner {
     {
         System.out.println("Exercicio 1");
 
-        Book book1 = new Book(100);
+        Book book1 = new Book();
         book1.setTitle("The Art Of Computer Programming");
-        Book book2 = new Book(75);
+        Book book2 = new Book();
         book2.setTitle("Effective Java");
-        Book book3 = new Book(60);
+        Book book3 = new Book();
         book3.setTitle("Clean Code");
 
         System.out.println(String.format( "Name: %s", book1.getTitle()));
         System.out.println(String.format( "Name: %s", book2.getTitle()));
         System.out.println(String.format( "Name: %s", book3.getTitle()));
-
-        System.out.println("\nExercicio 2");
-
-        book1.incrementVersion();
-        book2.incrementVersion();
-        book3.incrementVersion();
-        book3.incrementVersion();
-
-        System.out.println(String.format( "Name: %s, Version: %d", book1.getTitle(), book1.getVersion()));
-        System.out.println(String.format( "Name: %s, Version: %d", book2.getTitle(), book2.getVersion()));
-        System.out.println(String.format( "Name: %s, Version: %d", book3.getTitle(), book3.getVersion()));
-
-        System.out.println("\nExercicio 3");
-
-        System.out.println(String.format( "Name: %s, Version: %d, Pages: %d", book1.getTitle(), book1.getVersion(), book1.getPages()));
-        System.out.println(String.format( "Name: %s, Version: %d, Pages: %d", book2.getTitle(), book2.getVersion(), book2.getPages()));
-        System.out.println(String.format( "Name: %s, Version: %d, Pages: %d", book3.getTitle(), book3.getVersion(), book3.getPages()));
-
-        System.out.println("\nExercicio 4");
-
-        Author author1 = new Author("Donald E. Knuth", "donald@gmail.com", 214);
-        book1.setAuthor(author1);
-        Author author2 = new Author("Joshua Bloch", "joshua@gmail.com", 153);
-        book2.setAuthor(author2);
-        Author author3 = new Author("Robert C. Martin", "robert@gmail.com", 188);
-        book3.setAuthor(author3);
-
-        System.out.println(book1.toString());
-        System.out.println(book2.toString());
-        System.out.println(book3.toString());
-
-        System.out.println("\nExercicio 5");
-
-        Student student1 = new Student("Pedro Ribeiro", "pedro@gmail.com", 12345);
-        book1.setStudent(student1);
-
-        Student student2 = new Student("Ricardo Ferreira", "ricardo@gmail.com", 88982);
-        book2.setStudent(student2);
-
-        System.out.println(book1.toString());
-        System.out.println(book2.toString());
-        System.out.println(book3.toString());
-
-        System.out.println("\nExercicio 6");
-
-        book2.getLstReviews().add(new Review("Review 1", 3.4));
-        book2.getLstReviews().add(new Review("Review 2", 4.4));
-        book2.getLstReviews().add(new Review("Review 3", 5));
-        book2.getLstReviews().add(new Review("Review 4", 3.8));
-        book2.getLstReviews().add(new Review("Review 5", 3.9));
-
-        System.out.println(book2.toString());
     }
 }
--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,28 +1,8 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.OptionalDouble;
-
 public class Book {
 
     private String title;
-
-    private int version = 1;
-
-    private final int pages;
-
-    private Author author;
-
-    private Student student;
-
-    private boolean isBooked;
-
-    private List<Review> lstReviews = new ArrayList<>();
-
-    public Book(int pages) {
-        this.pages = pages;
-    }
 
     public String getTitle() {
         return title;
@@ -30,69 +10,5 @@ public class Book {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public int getVersion() {
-        return version;
-    }
-
-    public void incrementVersion() {
-       version++;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-        isBooked = true;
-    }
-
-    public boolean isBooked() {
-        return isBooked;
-    }
-
-    public List<Review> getLstReviews() {
-        return lstReviews;
-    }
-
-    public void setLstReviews(List<Review> lstReviews) {
-        this.lstReviews = lstReviews;
-    }
-
-    double reviewsAverage() {
-        int count = lstReviews.size();
-
-        if(count == 0) {
-            return 0.0;
-        }
-
-        double sum = 0;
-        for (int i = 0; i < count; i++){
-           sum += lstReviews.get(i).getReviewValue();
-        }
-
-       return sum / count;
-
-        //return lstReviews.stream().mapToInt(x -> x.getReviewValue()).average().orElse(0.0);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Name: %s, " +
-                "Version: %d, " +
-                "Pages: %d, " +
-                "%s" +
-                "Is Booked: %s, " +
-                "Review Average: %.2f", title, version, pages, author.toString(), isBooked ? String.format("Yes, Student: %s", student.getName()) : "No", reviewsAverage());
     }
 }
